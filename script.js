f25fa21ec2f471f96e087379a17290bc64cf81f2
Kyle (Linux) <kyle.j.blankenbeckler@gmail.com>
2020-01-22 20:35:34 -0600

    modularize

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 var arr=[];
+
 $(document).ready(function(){
     $('.ingredient').click(function(){
         var val = $(this).text();
+
         $(this).toggleClass('selected');
+
         if($(this).hasClass('selected')){
-            arr.push(val);
-            console.log(arr);
+            AddListItem(val);
         }
         else{
-            arr.splice($.inArray(val, arr),1);
-            console.log(arr);
+            RemoveListItem(val);
         }
-        $('#output-list').empty();
-        $.each(arr,function(i){
-            var li = $('<li/>')
-                .appendTo($('#output-list'));
-            var a = $('<a/>')
-                .text(arr[i])
-                .appendTo(li);
-        });
+
+        RefreshOutput();
+
+        FindCombos();
     });
 
 });
+
+function AddListItem(val){
+    arr.push(val);
+    //console.log(arr);
+}
+
+function RemoveListItem(val){
+    arr.splice($.inArray(val, arr),1);
+    //console.log(arr);
+}
+
+function RefreshOutput(){
+    $('#output-list').empty();
+    $.each(arr.sort(),function(i){
+        var li = $('<li/>')
+            .appendTo($('#output-list'));
+        var a = $('<a/>')
+            .text(arr[i])
+            .appendTo(li);
+    });
+}
